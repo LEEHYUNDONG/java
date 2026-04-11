91ae660587571fb2d6f1db368aab69b197a0c8b0
LEEHYUNDONG <l.steve10@yahoo.com>
2026-04-11 13:36:32 +0900

    feat: add event system with rate limiter and leetcode problems

---
+++ dojo/src/main/java/com/example/java/algorithm/leetcode/DeletedDupLinkedList.java
@@ -0,0 +1,40 @@
+package com.example.java.algorithm.leetcode;
+
+public class DeletedDupLinkedList {
+    class ListNode {
+        int val;
+        MergeTwoLists.ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, MergeTwoLists.ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null || head.next == null) return head;
+
+            ListNode prev = head;
+            ListNode curr = head.next;
+
+            while (curr != null) {
+                if (prev.val == curr.val) {
+                    prev.next = curr.next;
+                    curr = curr.next;
+                } else {
+                    prev = curr;
+                    curr = curr.next;
+                }
+            }
+            return head;
+        }
+    }
+}
